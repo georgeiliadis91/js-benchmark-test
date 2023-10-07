26bc85beaec2b08ef14880d978446b5c44233f2d
george iliadis <george.iliadis.91@gmail.com>
2023-10-08 02:25:06 +0300

    js immutability benchmark test

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("Giving time to run the perf test on the node console");
+
+setTimeout(() => {
+
+    const ARRA_SIZES = [100_000];
+
+    function intArray(n){
+        return new Array(n).fill(0).map((_,) => Math.floor(Math.random() * 100));
+    }
+
+    function addOneToEverything(arr){
+        /*
+        const newArr = [];
+        arr.forEach((item) => {
+            newArr.push(item+ 1);
+        });
+        */
+        const newArr = arr.reduce((acc, item) => {
+            return [...acc, item + 1];
+        }, []);
+        console.log(newArr);
+    }
+
+    (function (){
+        ARRA_SIZES.forEach((size) => {
+            const arr = intArray(size);
+            console.log (`Array size: ${size}`);
+            const t0 = performance.now();
+            addOneToEverything(arr);
+            const t1 = performance.now();
+            console.log(`Array size:  ${size}`);
+            console.log('Total time', t1 - t0)
+        });
+    })();
+
+}, 5000);
